f300ab6989bcb5c05cd86d1fa8c356085173403a
Razakasoa Nekena <nyavonekena6@gmail.com>
2025-08-07 12:19:24 +0300

    TitleMatch.java

--- Examen_POO/src/main/java/org/example/TitleMatch.java
+++ Examen_POO/src/main/java/org/example/TitleMatch.java
@@ -1,23 +1,23 @@
 package org.example;
 
 public class TitleMatch extends Match {
-    private String titre;
+    private String title;
 
     public TitleMatch(UFCfighter fighter1, UFCfighter fighter2, String titre) {
         super(fighter1, fighter2);
-        this.titre = titre;
+        this.title = titre;
     }
 
     @Override
-    public void terminer() {
+    public void finish() {
         if (points1 > points2) {
             fighter1.addVictory();
             fighter2.addDefeat();
-            fighter1.addTitle(titre);
+            fighter1.addTitle(title);
         } else if (points2 > points1) {
             fighter2.addVictory();
             fighter1.addDefeat();
-            fighter2.addTitle(titre);
+            fighter2.addTitle(title);
         } else {
             fighter1.addDraw();
             fighter2.addDraw();
